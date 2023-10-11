90c8ab5ac1a79fb2a21d6af853f83e249000cc03
Abhishek Verma <abhishekverma0203@gmail.com>
2023-10-11 10:06:37 +0530

    added sidebar to products page

---
+++ src/Components/SideBar/SideBar.tsx
@@ -0,0 +1,13 @@
+import "./sideBar.scss";
+
+function SideBar(){
+    return (
+      <>
+        <div className="sidebar">
+          Sidebar
+        </div>
+      </>
+    );
+}
+
+export default SideBar;
