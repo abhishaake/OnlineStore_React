b89e18482dde960d4564ef74555c1f253b5981fb
Abhishek Verma <abhishekverma0203@gmail.com>
2023-10-11 13:24:55 +0530

    Added Like button

--- src/Components/Products/Products.tsx
+++ src/Components/Products/Products.tsx
@@ -1,7 +1,17 @@
 import SideBar from "../SideBar/SideBar";
 import "./products.scss";
 import shirt from "./Assets/shirt.jpg";
+import Heart from "react-animated-heart";
+import { useState } from "react";
+
 function Products(){
+  const [isClick, setClick] = useState(new Array(12).fill(false));
+
+  const updateElement = (index:number) => {
+    const updatedArray = [...isClick]; 
+    updatedArray[index] = !updatedArray[index]; 
+    setClick(updatedArray); 
+  };
     return (
       <>
         <div className="products-bg">
@@ -15,52 +25,76 @@ function Products(){
           <div className="main">
             <div className="image-gallery">
               <span className="image-gallery-item">
+                <div className="image-gallery-item__heart"><Heart isClick={isClick[0]} onClick={() => updateElement(0)} /></div>
                 <img src={shirt}></img>
-                <span>Grey Shirt</span>
+                <br></br>
+                <span className="image-caption">Grey Shirt</span>
               </span>
               <span className="image-gallery-item">
+              <div className="image-gallery-item__heart"><Heart isClick={isClick[1]} onClick={() => updateElement(1)} /></div>
                 <img src={shirt}></img>
-                <span>Grey Shirt</span>
+                <br></br>
+                <span className="image-caption">Grey Shirt</span>
               </span>
               <span className="image-gallery-item">
+              <div className="image-gallery-item__heart"><Heart isClick={isClick[2]} onClick={() => updateElement(2)} /></div>
                 <img src={shirt}></img>
-                <span>Grey Shirt</span>
+                <br></br>
+                <span className="image-caption">Grey Shirt</span>
               </span>
               <span className="image-gallery-item">
+              <div className="image-gallery-item__heart"><Heart isClick={isClick[3]} onClick={() => updateElement(3)} /></div>
                 <img src={shirt}></img>
-                <span>Grey Shirt</span>
+                <br></br>
+                <span className="image-caption">Grey Shirt</span>
               </span>
               <span className="image-gallery-item">
+              <div className="image-gallery-item__heart"><Heart isClick={isClick[4]} onClick={() => updateElement(4)} /></div>
                 <img src={shirt}></img>
-                <span>Grey Shirt</span>
+                <br></br>
+                <span className="image-caption">Grey Shirt</span>
               </span>
               <span className="image-gallery-item">
+              <div className="image-gallery-item__heart"><Heart isClick={isClick[5]} onClick={() => updateElement(5)} /></div>
                 <img src={shirt}></img>
-                <span>Grey Shirt</span>
+                <br></br>
+                <span className="image-caption">Grey Shirt</span>
               </span>
               <span className="image-gallery-item">
+              <div className="image-gallery-item__heart"><Heart isClick={isClick[0]} onClick={() => updateElement(0)} /></div>
                 <img src={shirt}></img>
-                <span>Grey Shirt</span>
+                <br></br>
+                <span className="image-caption">Grey Shirt</span>
               </span>
               <span className="image-gallery-item">
+              <div className="image-gallery-item__heart"><Heart isClick={isClick[0]} onClick={() => updateElement(0)} /></div>
                 <img src={shirt}></img>
-                <span>Grey Shirt</span>
+                <br></br>
+                <span className="image-caption">Grey Shirt</span>
               </span>
               <span className="image-gallery-item">
+              <div className="image-gallery-item__heart"><Heart isClick={isClick[0]} onClick={() => updateElement(0)} /></div>
                 <img src={shirt}></img>
-                <span>Grey Shirt</span>
+                <br></br>
+                <span className="image-caption">Grey Shirt</span>
               </span>
               <span className="image-gallery-item">
+              <div className="image-gallery-item__heart"><Heart isClick={isClick[0]} onClick={() => updateElement(0)} /></div>
                 <img src={shirt}></img>
-                <span>Grey Shirt</span>
+                <br></br>
+                <span className="image-caption">Grey Shirt</span>
               </span>
               <span className="image-gallery-item">
+              <div className="image-gallery-item__heart"><Heart isClick={isClick[0]} onClick={() => updateElement(0)} /></div>
                 <img src={shirt}></img>
-                <span>Grey Shirt</span>
+                <br></br>
+                <span className="image-caption">Grey Shirt</span>
               </span>
               <span className="image-gallery-item">
+              <div className="image-gallery-item__heart"><Heart isClick={isClick[0]} onClick={() => updateElement(0)} /></div>
                 <img src={shirt}></img>
-                <span>Grey Shirt</span>
+                <br></br>
+                <span className="image-caption">Grey Shirt</span>
               </span>
 
             </div>
